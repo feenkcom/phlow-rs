7ed373b390b529ca1b5cd25746f40767d4e40f24
Aliaksei Syrel <alex.syrel@gmail.com>
2023-12-07 08:22:13 +0100

    add a bitmap view to the color example

--- phlow-examples/src/color.rs
+++ phlow-examples/src/color.rs
@@ -4,7 +4,17 @@
 #[macro_use]
 extern crate phlow;
 
-use phlow::PhlowView;
+use phlow::{PhlowBitmap, PhlowView};
+
+fn attenuate(myfloat: f32) -> u8 {
+    if myfloat > f32::from(u8::MAX) {
+        u8::MAX
+    } else if myfloat < f32::from(u8::MIN) {
+        u8::MIN
+    } else {
+        myfloat.round() as u8
+    }
+}
 
 #[derive(Clone)]
 pub struct Color {
@@ -23,6 +33,15 @@ impl Color {
             alpha: 1.0,
         }
     }
+
+    pub fn as_rgba(&self) -> Vec<u8> {
+        let red: u8 = attenuate(self.red);
+        let green: u8 = attenuate(self.green);
+        let blue: u8 = attenuate(self.blue);
+        let alpha: u8 = attenuate(self.alpha);
+
+        vec![red, green, blue, alpha]
+    }
 }
 
 define_extensions!(ExampleExtensions);
@@ -33,9 +52,9 @@ impl ColorExtensions {
     pub fn rgba_for(_this: &Color, view: impl PhlowView) -> impl PhlowView {
         view.list()
             .title("RGBA")
-            .items::<Color>(|color| {
-                phlow_all!(vec![color.red, color.green, color.blue, color.alpha])
-            })
+            .items::<Color>(
+                |color| phlow_all!(vec![color.red, color.green, color.blue, color.alpha])
+            )
             .item_text::<f32>(|each| each.to_string())
             .send::<f32>(|each| phlow!(each.clone()))
     }
@@ -60,6 +79,21 @@ impl ColorExtensions {
             .column_item::<(&str, f32)>("Value", |each| phlow!(each.1))
             .send::<(&str, f32)>(|each| phlow!(each.1))
     }
+
+    #[phlow::view]
+    pub fn preview_for(_this: &Color, view: impl PhlowView) -> impl PhlowView {
+        view.bitmap().title("Preview").bitmap::<Color>(|color| {
+            let width = 64i32;
+            let height = 64i32;
+            let pixels: Vec<u8> = color
+                .as_rgba()
+                .into_iter()
+                .cycle()
+                .take((width * height * 4) as usize)
+                .collect();
+            PhlowBitmap::rgba8(pixels, width, height)
+        })
+    }
 }
 
 import_extensions!(ExampleExtensions);
@@ -71,6 +105,11 @@ fn main() {
 
     let view = phlow!(color.clone())
         .phlow_view_named("components_for")
+        .unwrap();
+    println!("{}", view);
+
+    let view = phlow!(color.clone())
+        .phlow_view_named("preview_for")
         .unwrap();
     println!("{}", view);
 }
